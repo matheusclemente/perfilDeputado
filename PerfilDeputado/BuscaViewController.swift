1450f68a5dd4afb1f9ba6b7c0be3e1bb8456afd5
Matheus Clemente <matheus_klementt@hotmail.com>
2017-08-09 15:32:08 -0300

    Busca por range de data

---
+++ PerfilDeputado/BuscaViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BuscaViewController.swift
+//  PerfilDeputado
+//
+//  Created by Matheus Azevedo on 08/08/17.
+//  Copyright © 2017 Matheus Azevedo. All rights reserved.
+//
+
+import UIKit
+
+class BuscaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PerfilDeputado/VotacoesRequest.swift
@@ -0,0 +1,9 @@
+//
+//  VotacoesRequest.swift
+//  PerfilDeputado
+//
+//  Created by Matheus Azevedo on 09/08/17.
+//  Copyright © 2017 Matheus Azevedo. All rights reserved.
+//
+
+import Foundation
